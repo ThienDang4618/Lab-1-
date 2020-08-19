d57325130bd5f93b15367750ccd1e524fabe730f
ThienDang4618 <69919409+ThienDang4618@users.noreply.github.com>
2020-08-19 14:00:28 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+	public static void main (String[] args) { 
+		
+		System.out.println("Thien Dang");
+		System.out.println("Hello.java.");
+		System.out.println("Make sure java is installed and working"); 
+		System.out.println("08/19/2020");
+		System.out.println("CMSC 255-002");
+		System.out.println();
+	}
+}
